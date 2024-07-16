dfd49d5d728de3bf06dda3e338fbc1bbe0947aab
wangjinyou <web_wjy@xchl.xyz>
2024-07-16 17:39:48 +0800

    引入组件按需自动引入

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
   <h1 class="text-3xl font-bold underline">
     测试
   </h1>
+  <el-button type="primary">主要按钮</el-button>
 </template>
 
 <style scoped>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+// 自动导入 Element Plus 组件
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    AutoImport({
+      // element plus组件按需自动引入
+      resolvers: [ElementPlusResolver()],
+    }),
+    // vue 组件自动导入
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+});
+
