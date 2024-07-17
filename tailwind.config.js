710044d3d27bdcac791420c746e9d5e7d272e53a
wangjinyou <web_wjy@xchl.xyz>
2024-07-17 11:05:50 +0800

    更新daisyui配置

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,14 @@ export default {
     require('daisyui'),
   ],
   daisyui: {
-    themes: false, // 如果设为 true，则会包含所有的主题。 如果设为 false，则只有白天和黑暗模式的主题。 
+    themes: false, // 包含所有的主题
     darkTheme: "dark", // 默认暗黑主题
-    base: true, // 如果设为 true，一些基础样式 会被添加。
-    styled: true, // 如果设置为 true，组件会有默认的颜色和样式
-    utils: true, // 如果设为 true，响应式和工具类会被添加
-    prefix: "", // 如果设置为 true，所有的类名都会被添加前缀 
-    logs: false, // 如果设为 true，daisyUI 会在 CSS 构建时在命令行窗口输出日志。
-    themeRoot: ":root", // 选择器，用于设置主题变量 
+    base: true, // 添加基础样式
+    styled: true, // 组件默认的颜色和样式
+    utils: true, // 响应式和工具类添加
+    prefix: "", // 所有的类名添加前缀 
+    logs: false, // CSS 构建时在命令行窗口输出日志。
+    themeRoot: ":root", // 选择器，设置主题变量 
   },
 };
 
