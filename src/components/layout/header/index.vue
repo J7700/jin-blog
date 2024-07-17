5fc392edb2f5c682df274f84b1f62a1c63cb1cb0
wangjinyou <web_wjy@xchl.xyz>
2024-07-17 10:59:48 +0800

    安装并更新daisyui

--- src/components/layout/header/index.vue
+++ src/components/layout/header/index.vue
@@ -1,7 +1,39 @@
 <script setup></script>
 
 <template>
-  <div>头部</div>
+  <div class="header-wrap">
+    <div class="header-left">
+      左侧
+      <button class="btn btn-primary">Button</button>
+      <button class="btn w-64 rounded-full">Button</button>
+      <button class="btn btn-primary">One</button>
+      <button class="btn btn-secondary">Two</button>
+      <button class="btn btn-accent btn-outline">Three</button>
+    </div>
+    <div class="header-right">右侧</div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.header-wrap {
+  display: flex;
+  align-items: center;
+
+  .header-left {
+    width: 20%;
+
+    .logo {
+      width: 3rem;
+      height: 3rem;
+      transition: all 0.3s;
+    }
+  }
+
+  .header-right {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import { createApp } from "vue";
-import "./assets/common/css/base.scss";
 import App from "./App.vue";
+// 全局样式入口
+import "./assets/common/css/base.scss";
+// element-plus 暗黑主题
+import "element-plus/theme-chalk/dark/css-vars.css";
+// element-plus样式
+// import "element-plus/dist/index.css";
+// 引入路由
 import router from "./router/index.js";
+
 const app = createApp(App);
 
 app.use(router).mount("#app");
